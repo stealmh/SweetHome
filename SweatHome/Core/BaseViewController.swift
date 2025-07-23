ee6433d07251306345f9749d6da9a38e05a862d9
minhokim <kmh922@naver.com>
2025-07-23 21:02:35 +0900

    feat: BaseVC, BaseVM 추가 [#1]

---
+++ SweatHome/Core/BaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BaseViewController.swift
+//  SweatHome
+//
+//  Created by 김민호 on 7/23/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class BaseViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+        bind()
+    }
+    
+// MARK: - Setup Methods
+    func setupUI() { view.backgroundColor = .white }
+    func setupConstraints() {}
+    func bind() {}
+}
---
+++ SweatHome/Core/BaseViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  BaseViewModel.swift
+//  SweatHome
+//
+//  Created by 김민호 on 7/23/25.
+//
+
+import Foundation
+import RxSwift
+import RxRelay
+import RxCocoa
+
+class BaseViewModel: ViewModelable {
+    let disposeBag = DisposeBag()
+    
+    struct Input {}
+    
+    struct Output {}
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
+//MARK: - BaseViewModel Protocol
+private protocol ViewModelable {
+    associatedtype Input
+    associatedtype Output
+    
+    var disposeBag: DisposeBag { get }
+    
+    func transform(input: Input) -> Output
+}
+
+/// - Output에 붙여 사용: 로딩이 필요한 Presents에서 사용
+protocol ViewModelLoadable {
+    var isLoading: Driver<Bool> { get }
+}
+
+/// - Output에 붙여 사용: 네트워크 에러 처리가 필요한 Presents에서 사용
+protocol ViewModelErrorable {
+    var error: Driver<Error> { get }
+}
