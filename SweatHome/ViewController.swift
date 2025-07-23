c5a13d1bb08fa3bf132657c3d9743457fc5d1af9
minhokim <kmh922@naver.com>
2025-07-23 17:23:49 +0900

    Initial Commit

---
+++ SweatHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweatHome
+//
+//  Created by 김민호 on 7/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SweatHomeTests/SweatHomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SweatHomeTests.swift
+//  SweatHomeTests
+//
+//  Created by 김민호 on 7/23/25.
+//
+
+import Testing
+@testable import SweatHome
+
+struct SweatHomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
