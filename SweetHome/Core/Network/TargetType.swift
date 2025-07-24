cf0fb6287e03b5b2dd12f5eebdce3e66c2396028
minhokim <kmh922@naver.com>
2025-07-24 20:49:36 +0900

    feat: TargetType 구성 [#3]

---
+++ SweetHome/Core/Network/TargetType.swift
@@ -0,0 +1,31 @@
+//
+//  TargetType.swift
+//  SweetHome
+//
+//  Created by 김민호 on 7/24/25.
+//
+
+import Foundation
+import Alamofire
+
+protocol TargetType {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var parameters: [String: Any]? { get }
+    var encoding: ParameterEncoding { get }
+    var multipartData: [MultipartFormData]? { get }
+}
+
+extension TargetType {
+    var url: String { return baseURL + path }
+    
+    var headers: [String: String]? { return ["Content-Type": "application/json"] }
+    
+    var parameters: [String: Any]? { return nil }
+    
+    var encoding: ParameterEncoding { return JSONEncoding.default }
+    
+    var multipartData: [MultipartFormData]? { return nil }
+}
