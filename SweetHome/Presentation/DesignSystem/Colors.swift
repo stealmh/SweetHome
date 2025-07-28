023cefa2a6109f67f14b74bd1cb20d23061a15bf
minhokim <kmh922@naver.com>
2025-07-28 15:19:53 +0900

    design: Colorset 구성 [#5]

---
+++ SweetHome/Presentation/DesignSystem/Colors.swift
@@ -0,0 +1,30 @@
+//
+//  Colors.swift
+//  SweetHome
+//
+//  Created by 김민호 on 7/28/25.
+//
+
+import UIKit
+
+enum SHColor {
+    enum Brand {
+        static let deepCream = UIColor(hex: "#DAC8B1")
+        static let brightCream = UIColor(hex: "#F1DCC1")
+        static let deepCoast = UIColor(hex: "#298E9C")
+        static let brightCoast = UIColor(hex: "#71C9D6")
+        static let deepWood = UIColor(hex: "#402A32")
+        static let brightWood = UIColor(hex: "#8C5543")
+    }
+    
+    enum GrayScale {
+        static let gray_0 = UIColor(hex: "#FFFFFF")
+        static let gray_15 = UIColor(hex: "#F9F9F9")
+        static let gray_30 = UIColor(hex: "#EAEAEA")
+        static let gray_45 = UIColor(hex: "#D8D6D7")
+        static let gray_60 = UIColor(hex: "#ABABAE")
+        static let gray_75 = UIColor(hex: "#6A6A6E")
+        static let gray_90 = UIColor(hex: "#434347")
+        static let gray_100 = UIColor(hex: "#0B0B0B")
+    }
+}
