bbb27f8e395fab6745fff4f113c81d6a4d07c502
minhokim <kmh922@naver.com>
2025-08-03 18:47:44 +0900

    feat:  APIConstants 구성 [#9]

--- SweetHome/Core/Constants/Constants.swift
+++ SweetHome/Core/Constants/Constants.swift
@@ -2,6 +2,12 @@
 
 import Foundation
 
+// MARK: - API Configuration
+struct APIConstants {
+    static let baseURL = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as! String
+    static let sesacKey = Bundle.main.object(forInfoDictionaryKey: "SESAC_KEY") as! String
+}
+
 // MARK: - Notification Names
 extension Notification.Name {
     static let refreshTokenExpired = Notification.Name("refreshTokenExpired")
