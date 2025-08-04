07144cc821c1a9ad0e88db98f71e806fe308c5c2
minhokim <kmh922@naver.com>
2025-08-04 20:39:42 +0900

    chore: EstateSectionHeaderView identifier 추가 [#9]

--- SweetHome/Presentation/Home/View/Components/EstateSectionHeaderView.swift
+++ SweetHome/Presentation/Home/View/Components/EstateSectionHeaderView.swift
@@ -9,7 +9,7 @@ import UIKit
 import SnapKit
 
 class EstateSectionHeaderView: UICollectionReusableView {
-    
+    static let identifier = "EstateSectionHeaderView"
     // MARK: - Properties
     var onViewAllTapped: (() -> Void)?
     
