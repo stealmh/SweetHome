cc8544957464bfdea4d254e6afd15f15e6fbd07b
minhokim <kmh922@naver.com>
2025-08-05 14:29:49 +0900

    feat: Estate 검색 결과 화면 생성 [#11]

---
+++ SweetHome/Presentation/Home/View/EstateSearchResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EstateSearchResultViewController.swift
+//  SweetHome
+//
+//  Created by 김민호 on 8/5/25.
+//
+
+import UIKit
+
+/// 검색 후 리스트 형식으로 보여지는 결과 화면
+class EstateSearchResultViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
