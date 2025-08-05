65bd6ead35decb07a12ad93e550d5f900f981dc8
minhokim <kmh922@naver.com>
2025-08-05 14:29:41 +0900

    feat: Estate 지도 화면 생성 [#11]

---
+++ SweetHome/Presentation/EstateMap/EstateMapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EstateMapViewController.swift
+//  SweetHome
+//
+//  Created by 김민호 on 8/5/25.
+//
+
+import UIKit
+
+class EstateMapViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    override func setupUI() {
+        super.setupUI()
+    }
+    
+    override func setupConstraints() {
+
+    }
+}
