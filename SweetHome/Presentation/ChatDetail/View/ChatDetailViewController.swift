653485c1c14f5c49e6550c7d599d5a7a60fbfd2b
minhokim <kmh922@naver.com>
2025-08-25 14:45:05 +0900

    rename: ChatRoom -> ChatDetail 이름변경

--- SweetHome/Presentation/ChatDetail/View/ChatDetailViewController.swift
+++ SweetHome/Presentation/ChatDetail/View/ChatDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ChatRoomDetailViewController.swift
+//  ChatDetailViewController.swift
 //  SweetHome
 //
 //  Created by 김민호 on 8/25/25.
@@ -10,7 +10,7 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-class ChatRoomDetailViewController: BaseViewController {
+class ChatDetailViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- SweetHome/Presentation/ChatDetail/ViewModel/ChatDetailViewModel.swift
+++ SweetHome/Presentation/ChatDetail/ViewModel/ChatDetailViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  ChatRoomDetailViewModel.swift
+//  ChatDetailViewModel.swift
 //  SweetHome
 //
 //  Created by 김민호 on 8/25/25.
