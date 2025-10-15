527bef23d930ba5ccd749dcb8ef7150a8ed6c12b
minhokim <kmh922@naver.com>
2025-10-15 16:03:26 +0900

    refactor: Home 로직 클린아키텍쳐 적용 [#30]

---
+++ SweetHome/Domain/Repositories/EstateRepository.swift
@@ -0,0 +1,21 @@
+//
+//  EstateRepository.swift
+//  SweetHome
+//
+//  Created by 김민호 on 10/15/25.
+//
+
+import Foundation
+import RxSwift
+
+/// - 매물 관련 데이터 접근을 추상화하는 Repository
+protocol EstateRepository {
+    /// - 오늘의 매물 목록 조회
+    func fetchTodayEstates() -> Observable<[Estate]>
+
+    /// - 인기 매물 목록 조회
+    func fetchHotEstates() -> Observable<[Estate]>
+
+    /// - 오늘의 부동산 토픽 조회
+    func fetchTopics() -> Observable<[EstateTopic]>
+}
---
+++ SweetHome/Domain/UseCases/HomeUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  HomeUseCase.swift
+//  SweetHome
+//
+//  Created by 김민호 on 10/15/25.
+//
+
+import Foundation
+import RxSwift
+
+/// - 홈 화면 관련 비즈니스 로직을 추상화하는 UseCase
+protocol HomeUseCase {
+    /// - 오늘의 매물 목록 조회
+    func fetchTodayEstates() -> Observable<[Estate]>
+
+    /// - 인기 매물 목록 조회
+    func fetchHotEstates() -> Observable<[Estate]>
+
+    /// - 오늘의 부동산 토픽 조회
+    func fetchTopics() -> Observable<[EstateTopic]>
+}
