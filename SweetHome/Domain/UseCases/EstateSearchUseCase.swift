262ec30cd106903f584dfdc261c091af44ac291c
minhokim <kmh922@naver.com>
2025-10-15 16:23:46 +0900

    refactor: Estate Search 로직 클린아키텍쳐 적용 [#30]

---
+++ SweetHome/Domain/UseCases/EstateSearchUseCase.swift
@@ -0,0 +1,16 @@
+//
+//  EstateSearchUseCase.swift
+//  SweetHome
+//
+//  Created by 김민호 on 10/15/25.
+//
+
+import Foundation
+import RxSwift
+
+/// - 매물 검색 관련 비즈니스 로직을 추상화하는 UseCase
+protocol EstateSearchUseCase {
+    /// - 매물 검색
+    /// - Parameter query: 검색어
+    func searchEstates(query: String) -> Observable<[Estate]>
+}
